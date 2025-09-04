7f7e91b6c59ff4170b4e724f79469b718585953b
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2025-09-04 04:32:17 -0700

    Successfully created login and signup functionality with firebase

--- util/auth.js
+++ util/auth.js
@@ -1,15 +1,25 @@
 import axios from "axios"
 
 
-const API_Key = 'AIzaSyBH4T7oQs7VlLHiAB1fZ0AE1dZ601gYg5'
-export async function createUser(email, password){
-    const response = axios.post(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_Key,
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }
+const API_Key = 'AIzaSyBH4T7oQs7VlLHiAB1fZ0AE1dZ601gYg5U'
 
-    );
+
+async function authenticate(mode, email, password){
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_Key}`;
+    
+    const response = await axios.post(url, {
+            email: email,
+            password: password,  
+            returnSecureToken: true
+    })
+
+    console.log(response.data);
 }
+
+export async function createUser(email, password){
+    await authenticate('signUp', email, password);
+}
+
+export async function login(email, password){
+    await authenticate('signInWithPassword', email, password);
+} 
